e565723bee1e3f2d711583253587117bd4f8b2f6
stefanocoretta <stefano.coretta@gmail.com>
2023-12-24 13:40:07 +0000

    WIP process expex gloss

--- _extensions/interlinear/interlinear.lua
+++ _extensions/interlinear/interlinear.lua
@@ -68,5 +68,57 @@ function Div(div)
         end
       end
     end
+
+    if FORMAT:match "latex" or FORMAT:match "beamer" then
+      -- collect existing identifiers and classes to be added back below
+      local div_identifiers = div.identifier
+      local div_classes = div.classes
+
+      for _, block in ipairs(div.content) do
+        if block.t == "LineBlock" then
+          local preamble_n = 0
+          local paragraphs = {}
+          table.insert(paragraphs, pandoc.RawInline('tex', '\\ex\n\\begingl\n'))
+          for i, inline in ipairs(block.content) do
+            local para = pandoc.RawInline('tex', pandoc.utils.stringify(inline))
+            -- top lines marked with "-" are original gloss lines
+            -- a bottom line with only "-" creates a gloss without free translation
+              -- TODO: strip empty <p> when there is no free translation
+            -- #para.content > 0 needed for empty LineBlock lines
+            if para.text ~= "" and string.sub(para.text, 1, 1) == "-" then
+              preamble_n = preamble_n + 1
+              -- strip leading "-"
+              para.text = string.gsub(para.text, "^- ", "")
+              para.text = "\\glpreamble " .. para.text .. " //\n"
+              
+            elseif i == preamble_n + 1 then
+              para.text = "\\gla " .. para.text .. " //\n"
+            -- if para is last line, quote it if it's not already quoted
+            elseif para.text ~= "" and i == #block.content then
+              para.text = "\\glft `" .. para.text .. "' //\n"
+            end
+
+            table.insert(paragraphs, para)
+          end
+          table.insert(paragraphs, pandoc.RawInline('tex', '\\endgl\n\\xe'))
+
+          local concatenated_text = ""
+          for _, inline in ipairs(paragraphs) do
+              concatenated_text = concatenated_text .. pandoc.utils.stringify(inline.text)
+          end
+
+          -- Create a new Para block with the concatenated text
+          local paragraphs_cat = pandoc.RawInline("tex", concatenated_text)
+
+          div = pandoc.Div(paragraphs_cat)
+          quarto.log.output(paragraphs_cat)
+
+          -- add back identifiers and classes
+          div.identifier = div_identifiers
+          div.classes = div_classes
+          return div
+        end
+      end
+    end
   end
 end
