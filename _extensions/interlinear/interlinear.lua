229cc0fb2132ae561e6470445f10a400c7a409de
stefanocoretta <stefano.coretta@gmail.com>
2023-12-24 10:25:47 +0000

    add support for original line in gloss

--- _extensions/interlinear/interlinear.lua
+++ _extensions/interlinear/interlinear.lua
@@ -1,9 +1,9 @@
 -- Include Leipzig.js
 function Meta(meta)
   meta["header-includes"] = pandoc.RawBlock("html", '<link rel="stylesheet" href="//unpkg.com/leipzig/dist/leipzig.min.css">')
-  local mia = meta["include-after"]
-  table.insert(mia, 2, pandoc.RawBlock("html", '<script src="//unpkg.com/leipzig/dist/leipzig.min.js"></script>'))
-  table.insert(mia, 3, pandoc.RawBlock("html", [[
+  local meta_include_after = meta["include-after"]
+  table.insert(meta_include_after, 2, pandoc.RawBlock("html", '<script src="//unpkg.com/leipzig/dist/leipzig.min.js"></script>'))
+  table.insert(meta_include_after, 3, pandoc.RawBlock("html", [[
 <script>
 document.addEventListener('DOMContentLoaded', function() {
   Leipzig().gloss();
@@ -16,14 +16,37 @@ end
 function Div(div)
   if FORMAT:match "html" then
     if div.classes:includes("ex") then
+      -- collect existing identifiers and classes to be added back below
+      local div_identifiers = div.identifier
+      local div_classes = div.classes
+
       for _, block in ipairs(div.content) do
         if block.t == "LineBlock" then
           local paragraphs = {}
           for _, inline in ipairs(block.content) do
-            table.insert(paragraphs, pandoc.Para(inline))
+            local para = pandoc.Para(inline)
+            -- lines marked with "-" are original gloss lines
+            -- #para.content > 0 needed for empty LineBlock lines
+            if #para.content > 0 and pandoc.utils.stringify(para.content[1]) == "-" then
+              -- strip leading "-"
+              table.remove(para.content, 1)
+              -- Element Para cannot have classes, so we use a Plain element
+              para = pandoc.Plain(
+                  {pandoc.RawInline('html', '<p class="gloss__line--original">')}
+                  .. para.content ..
+                  {pandoc.RawInline('html', '</p>')}
+                )
+            end
+            table.insert(paragraphs, para)
           end
+
           div = pandoc.Div(paragraphs)
+          -- add necessary attribute for leipzig.js to process the div
           div.attributes["data-gloss"] = ""
+
+          -- add back identifiers and classes
+          div.identifier = div_identifiers
+          div.classes = div_classes
           return div
         end
       end
